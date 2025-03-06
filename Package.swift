784dbf3c11f45db49c4b2ed857dfc364aad0908e
Tez Park <tez.park@sendbird.com>
2025-03-06 17:13:54 +0900

    [MOD] modified package

--- Package.swift
+++ Package.swift
@@ -5,17 +5,22 @@ let package = Package(
     name: "SendbirdAIAgent",
     platforms: [.iOS(.v13)],
     products: [
-        .library(name: "SendbirdAIAgent", targets: ["SendbirdAIAgent"])
+        .library(name: "SendbirdAIAgent", targets: ["SendbirdAIAgent", "SendbirdAIAgentSupport"])
     ],
     dependencies: [
         // Add GitHub repository dependency
-        .package(name: "AIAgentCore", url: "https://github.com/tezpark/module-test-ios.git", from: "1.0.10")
+        .package(name: "AIAgentCore", url: "https://github.com/tezpark/module-test-ios.git", from: "1.0.11")
     ],
     targets: [
         .binaryTarget(
             name: "SendbirdAIAgent",
-            url: "https://github.com/tezpark/sub-module-test-ios/raw/netflix/1.0.0/Frameworks/SendbirdAIAgent.xcframework.zip",
-            checksum: "063642a8c4389d1d071ef4a84e4ee8dd739d991dc6a6a8d2412fee4de4a7d4d4"
+            path: "Frameworks/SendbirdAIAgent.xcframework.zip"
+        ),
+        .target(
+            name: "SendbirdAIAgentSupport",
+            dependencies: [
+                .product(name: "AIAgentCore", package: "AIAgentCore")
+            ]
         )
     ]
 )
---
+++ Sources/SendbirdAIAgentSupport/SendbirdAIAgent.swift
@@ -0,0 +1,7 @@
+//
+//  SendbirdAIAgent.swift
+//  SendbirdAIAgent
+//
+//  Created by Tez Park on 3/6/25.
+//
+
